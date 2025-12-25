ae5e6188267b756ce18770c043e02a67bb38bb86
Matt Folsom <msf930@gmail.com>
2025-12-24 19:16:39 -0700

    ios safe area black

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,11 @@ export const metadata: Metadata = {
   title: "Footy Elite Soccer Coaching",
   description: "Footy Elite Soccer Coaching is a soccer coaching company that provides one-on-one training for young athletes. We are committed to helping players build strong fundamentals, confidence, and a genuine love for the game as they work toward reaching their full potential.",
   keywords: "soccer coaching, one-on-one training, young athletes, soccer training, soccer development, soccer skills, soccer training programs, soccer training methods, soccer training techniques, soccer training drills, soccer training exercises, soccer training programs, soccer training methods, soccer training techniques, soccer training drills, soccer training exercises",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    viewportFit: "cover",
+  },
 };
 
 export default function RootLayout({
